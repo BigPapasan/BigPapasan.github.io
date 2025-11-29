4bd153c545225d48ca00764c6c5e512a66a65358
VDH-Design <twvanderhorst@gmail.com>
2025-11-29 10:36:12 -0600

    Add files via upload

---
+++ Italy/Italy Trip/Italy Trip/Italy_TripApp.swift
@@ -0,0 +1,17 @@
+//
+//  Italy_TripApp.swift
+//  Italy Trip
+//
+//  Created by Ted VanDerHorst on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Italy_TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Italy/Italy Trip/ItalyTrip/ItalyTripApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct ItalyTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ItalyHomeView()
+        }
+    }
+}
